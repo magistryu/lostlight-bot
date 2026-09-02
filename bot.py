d061125883658d9cb2ed0318ca7e4cb0b2197a2c
magistryu <sara494825@gmail.com>
2026-09-02 12:44:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.run_webhook(
         listen="0.0.0.0",
-        port=int(os.getenv("PORT", 8080)),
+        port=int(os.getenv("PORT", 8000)),
         webhook_url=WEBHOOK_URL + "/webhook"
     )
 
